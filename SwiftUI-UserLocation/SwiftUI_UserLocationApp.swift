8901915fbec3def97a2dace3800fd9e65797eea3
KevinJasinghe <kevinjasinghe@gmail.com>
2022-04-03 11:51:08 -0400

    Initial Commit

---
+++ SwiftUI-UserLocation/SwiftUI_UserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UserLocationApp.swift
+//  SwiftUI-UserLocation
+//
+//  Created by Kevin Jasinghe on 2022-04-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
